746f0cb128d56996ee63b2f6176d4bf5287c030e
Surya-7890 <suryamariappan9108@gmail.com>
2024-04-26 20:26:24 +0530

    loadbalancer

---
+++ main.go
@@ -0,0 +1,43 @@
+package loadbalancer
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+)
+
+var (
+	ProxyList   = make(map[string][]*httputil.ReverseProxy)
+	recentProxy = make(map[string]uint8)
+)
+
+/* @param hosts example "http://localhost:7000"  */
+/* @param path example "/api"  */
+func CreateNewProxy(path string, hosts ...string) {
+	for _, host := range hosts {
+		newProxy := httputil.NewSingleHostReverseProxy(&url.URL{
+			Scheme: strings.Split(host, ":")[0],
+			Host:   strings.Split(host, "://")[1],
+		})
+		ProxyList[path] = append(ProxyList[path], newProxy)
+	}
+}
+
+func handleProxy(w http.ResponseWriter, r *http.Request, path string) {
+	remainder := uint8(len(ProxyList[path]))
+	ProxyList[path][(recentProxy[path]+1)%remainder].ServeHTTP(w, r)
+	defer func() {
+		recentProxy[path] = (recentProxy[path] + 1) % remainder
+	}()
+}
+
+func StartServer() {
+	for k := range ProxyList {
+		http.HandleFunc(k, func(w http.ResponseWriter, r *http.Request) {
+			handleProxy(w, r, k)
+		})
+	}
+
+	http.ListenAndServe(":8000", nil)
+}
